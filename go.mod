fdee3a2eb0667394f6c2ca89816318bdfd957ad4
vearne <asdwoshiaotian@gmail.com>
2023-12-19 12:47:01 +0800

    add zaplog.go

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/vearne/zaplog
+
+go 1.21.0
+
+require go.uber.org/zap v1.26.0
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
---
+++ zaplog.go
@@ -0,0 +1,68 @@
+package zaplog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var (
+	DefaultLogger *zap.Logger
+)
+
+func InitLogger(logPath string, level string) {
+	alevel := zap.NewAtomicLevel()
+
+	hook := lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    1024, // megabytes
+		MaxBackups: 10,
+		MaxAge:     7,    //days
+		Compress:   true, // disabled by default
+	}
+	w := zapcore.AddSync(&hook)
+
+	switch level {
+	case "debug":
+		alevel.SetLevel(zap.DebugLevel)
+	case "info":
+		alevel.SetLevel(zap.InfoLevel)
+	case "error":
+		alevel.SetLevel(zap.ErrorLevel)
+	default:
+		alevel.SetLevel(zap.InfoLevel)
+	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05")
+	encoderConfig.ConsoleSeparator = " | "
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		w,
+		alevel,
+	)
+
+	DefaultLogger = zap.New(core)
+	DefaultLogger = DefaultLogger.WithOptions(zap.AddCaller(), zap.AddCallerSkip(1))
+}
+
+func Debug(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Debug(msg, fields...)
+}
+
+func Info(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Fatal(msg, fields...)
+}
