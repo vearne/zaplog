8f76c88fcdadd7a83404a208f2d4e6961652272d
vearne <asdwoshiaotian@gmail.com>
2024-05-22 17:32:44 +0800

    update zaplog.go

--- zaplog.go
+++ zaplog.go
@@ -56,6 +56,7 @@ func InitLogger(logPath string, level string) {
 	DefaultLogger = otelzap.New(logger,
 		otelzap.WithTraceIDField(true),
 		otelzap.WithMinLevel(zap.DebugLevel),
+		otelzap.WithCallerDepth(1),
 	)
 }
 
